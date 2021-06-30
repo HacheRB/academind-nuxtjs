b2a516c8daa9a48503b28b12e69085e2f6dfdadd
Hector Rodriguez Bravo <61188167+HacheRB@users.noreply.github.com>
2021-06-30 22:48:59 +0100

    Section 2 SideNav not closing

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSideNav
+      :show="displaySidenav"
+      @close="displaySideNav = false"
+    />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader.vue'
+import TheSideNav from '@/components/Navigation/TheSidenav.vue'
+
+export default {
+  components: {
+    TheHeader, TheSideNav
+  },
+  data () {
+    return {
+      displaySidenav: false
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
